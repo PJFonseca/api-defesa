886c73fa811165645fb02423d75a9f0788ac29f9
Pedro Fonseca <pedro+github@fonseca.com.pt>
2026-06-27 12:44:35 +0000

    DEFESA API

--- pessoas/api.py
+++ pessoas/api.py
@@ -14,21 +14,26 @@ api = NinjaAPI(
 ###########################################################
 # listar - corrigir !!!!
 ###########################################################
-@api.get("pessoas/", 
-         response={200: List[PessoaOut]}, 
-         description="Lista todas as pessoas")
-def listar_pessoas(request,  
-                 sort: str = None, 
-                 idade: int = None,
-                 nome: str = None,
-                 offset: int = 0,
-                 limit: int = 3 ):
-    
+@api.get("pessoas/",
+         response={200: List[PessoaOut]},
+         description="Lista todas as pessoas",
+         tags=["Pessoas"])
+def listar_pessoas(request,
+                   sort: str = None,
+                   idade: int = None,
+                   nome: str = None,
+                   offset: int = 0,
+                   limit: int = 3):
     pessoas = Pessoa.objects.all()
-    
+
     if idade:
         pessoas = pessoas.filter(idade=idade)
-    
+    if nome:
+        pessoas = pessoas.filter(nome=nome)
+    if sort:
+        pessoas = pessoas.order_by(sort)
+
+    pessoas = pessoas[offset:offset + limit]
     return 200, pessoas
 
 
@@ -37,22 +42,23 @@ def listar_pessoas(request,
 # ver um - corrigir !!!!
 ###########################################################
 @api.get("pessoas/{pessoa_id}/", 
-         response={200: List[PessoaOut], 404: ErrorSchema}, 
-         description="Ver dados de uma pessoa"
+         response={200: PessoaOut, 404: ErrorSchema}, 
+         description="Ver dados de uma pessoa",
+         tags=["Pessoas"]
          )
 def ver_uma_pessoa(request, pessoa_id:int):
     try:
-        return 200, Pessoa.objects.all()
-    except:
-        pass
-        
+        return 200, Pessoa.objects.get(id=pessoa_id)
+    except Pessoa.DoesNotExist:
+        return 404, {"detail": "Pessoa não encontrada"}
+
 
 ###########################################################
 # criar - não mexer, estao corretos
 ###########################################################
 @api.post("pessoas/", 
           response={201: PessoaOut}, 
-         tags=["Pessoas"],
+          tags=["Pessoas"],
           description="Criar uma nova pessoa")
 def criar_uma_pessoa(request, data: PessoaIn):
     return 201, Pessoa.objects.create(**data.dict())
@@ -67,15 +73,28 @@ def criar_uma_pessoa(request, data: PessoaIn):
          description="Substituir os dados duma pessoa")
 def atualizar_uma_pessoa(request, pessoa_id: int, data: PessoaIn):
     try:
-        pessoa = Pessoa.objects.filter(id=pessoa_id).update(**data.dict())
+        pessoa = Pessoa.objects.get(id=pessoa_id)
+        for attr, value in data.dict().items():
+            setattr(pessoa, attr, value)
+        pessoa.save()
         return 200, pessoa
-    except:
+    except Pessoa.DoesNotExist:
         return 404, {"detail": "Pessoa não encontrada"}
 
 
 ###########################################################
 # apagar - implementar !!!!
 ###########################################################
-
+@api.delete("pessoas/{pessoa_id}/", 
+            response={204: None, 404: ErrorSchema}, 
+            description="Apagar uma pessoa",
+            tags=["Pessoas"])
+def apagar_uma_pessoa(request, pessoa_id: int):
+    try:
+        pessoa = Pessoa.objects.get(id=pessoa_id)
+        pessoa.delete()
+        return 204, {"detail": "Pessoa apagada com sucesso"}
+    except Pessoa.DoesNotExist:
+        return 404, {"detail": "Pessoa não encontrada"}
 
 
--- pessoas/schemas.py
+++ pessoas/schemas.py
@@ -2,6 +2,7 @@ from ninja import Schema
 
 class PessoaIn(Schema):
     nome: str 
+    idade: int
 
 class PessoaOut(PessoaIn):
     id: int
